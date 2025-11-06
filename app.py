21a63c7550e85f8f684c6a8a2f3838eb707c36f7
00400622-hub <00400622@uca.edu.sv>
2025-11-06 16:42:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from modulos.venta import mostrar_venta
-from modulos.compras import mostrar_compras
-from modulos.clientes import mostrar_clientes
+
 
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
     seleccion = st.sidebar.selectbox("Selecciona una opción", ["Ventas", "Compras","Clientes"])
