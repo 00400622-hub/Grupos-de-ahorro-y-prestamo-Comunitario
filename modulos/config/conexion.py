f42931b73f34c5b8d33f1c43f79a31ed14fe66e8
00400622-hub <00400622@uca.edu.sv>
2025-11-09 17:23:00 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -1,21 +1,13 @@
-import mysql.connector
-from mysql.connector import Error
+import os, mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
 
 def obtener_conexion():
-    try:
-        conexion = mysql.connector.connect(
-            host='bddu6yel2ww6hx27qwg0-mysql.services.clever-cloud.com',
-            user='uvkxd9piyuwt9e3d',
-            password='NVcd1m955q5Qrzei5rFt',
-            database='bddu6yel2ww6hx27qwg0',
-            port=3306
-        )
-        if conexion.is_connected():
-            print("✅ Conexión establecida")
-            return conexion
-        else:
-            print("❌ Conexión fallida (is_connected = False)")
-            return None
-    except mysql.connector.Error as e:
-        print(f"❌ Error al conectar: {e}")
-        return None
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        autocommit=False
+    )
